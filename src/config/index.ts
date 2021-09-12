f1a78a929d19b872361375382ae8ae99ef42d92e
sinchang <sinchangwen@gmail.com>
2021-09-12 15:33:16 +0800

    Add Environment Variables

---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+export const CLIENT_VAR = process.env.NEXT_PUBLIC_CLIENT_VAR
+export const SERVER_VAR = process.env.SERVER_VAR
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
+import { CLIENT_VAR, SERVER_VAR } from '@/config'
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
+  console.log(SERVER_VAR)
+  useEffect(() => {
+    console.log(CLIENT_VAR)
+  }, [])
+
   const hello = 'Hello World'
   return <div>{hello}</div>
 }
